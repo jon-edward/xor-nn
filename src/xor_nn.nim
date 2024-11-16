d4c2a463cdc0fa166529be4dab384cc425511fd3
jon-edward <townsend.jonathan.e@gmail.com>
2024-11-15 21:02:16 -0500

    Initial

---
+++ src/xor_nn.nim
@@ -0,0 +1,10 @@
+# This is just an example to get you started. A typical binary package
+# uses this file as the main entry point of the application.
+
+const layerHeight = 8
+const layerWidth = 8
+
+var nn: array[layerWidth, array[layerHeight, uint32]]
+
+echo nn
+
---
+++ xor_nn.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "jon-edward"
+description   = "XOR neural network"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["xor_nn"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.0"
